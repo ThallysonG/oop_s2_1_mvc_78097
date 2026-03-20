f1cc0afb4a749691add1a29ed2c9cdd80dcebde3
Thallyson Gama <104468566+ThallysonG@users.noreply.github.com>
2026-03-20 13:20:08 +0000

    Add project files.

---
+++ oop_s2_1_mvc_78097/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace oop_s2_1_mvc_78097.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
