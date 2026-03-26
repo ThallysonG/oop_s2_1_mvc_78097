8f737cf52523625b2a18f475816f9aa9d26e0e34
Thallyson Gama <104468566+ThallysonG@users.noreply.github.com>
2026-03-26 12:26:33 +0000

    Delete oop_s2_1_mvc_78097 directory
    old files

--- oop_s2_1_mvc_78097/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace oop_s2_1_mvc_78097.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
